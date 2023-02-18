eae68c2081458637784ffb9f474ef98fd9b5cce3
Karan Kumar <karankumar072508@gmail.com>
2023-02-18 19:11:26 +0530

    adding the styling to our home page.

--- inchat/src/pages/home.js
+++ inchat/src/pages/home.js
@@ -1,47 +1,48 @@
+import styles from '../styles/home.module.css';
 
 const Home = () =>{
     return(
         <div className="post-list">
-            <div className="post-wrapper">
-                <div className="post-header">
-                    <div className="post-avatar">
+            <div className={styles.postWrapper}>
+                <div className={styles.postHeader}>
+                    <div className={styles.postAvatar}>
                         <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="user-profile-pic"></img>
 
-                        <div className="author-details">
-                            <span className="post-author">Karan kr</span>
-                            <span className="post-time"> a minute ago</span>
+                        <div className={styles.postAuthor}>
+                            <span className={styles.postAuthor}>Karan kr</span>
+                            <span className={styles.postTime}> a minute ago</span>
                         </div>
                     </div>
 
-                    <div className="post-content">
+                    <div className={styles.postContent}>
                         Post Content
                     </div>
 
-                    <div className="post-action">
-                        <div className="post-like">
+                    <div className={styles.postActions}>
+                        <div className={styles.postLike}>
                             <img src="https://cdn-icons-png.flaticon.com/512/535/535234.png" alt="like-icon"></img>
                             <span>5</span>
                         </div>
 
-                        <div className="post-comment-icon">
+                        <div className={styles.postCommentsIcon}>
                             <img src="https://cdn-icons-png.flaticon.com/512/13/13673.png" alt="comments-icon"></img>
                             <span>3</span>
                         </div>
                     </div>
 
-                    <div className="post-comment-box">
+                    <div className={styles.postCommentBox}>
                         <input placeholder="start typing comment" />
                     </div>
 
-                    <div className="post-comments-list">
-                        <div className="post-comments-item">
-                            <div className="post-comment-header">
-                                <span className="post-commment-author">Karan</span>
-                                <span className="post-comment-time">a minute ago</span>
-                                <span className="post-comments-like">23</span>
+                    <div className={styles.postCommentsList}>
+                        <div className={styles.postCommentItem}>
+                            <div className={styles.postCommentHeader}>
+                                <span className={styles.postCommentAuthor}>Karan</span>
+                                <span className={styles.postCommentTime}>a minute ago</span>
+                                <span className={styles.postCommentLikes}>23</span>
                             </div>
 
-                            <div className="post-comment-content">Random comment</div>
+                            <div className={styles.postCommentContent}>Random comment</div>
                         </div>
                     </div>
                 </div>
