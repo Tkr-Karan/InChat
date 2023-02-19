d482449603b7f4bcc56c195b6d44c1f4fb53764a
Karan Kumar <karankumar072508@gmail.com>
2023-02-19 18:38:29 +0530

    Adding and Creating Loader for page

--- inchat/src/components/App.js
+++ inchat/src/components/App.js
@@ -1,14 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {getPosts} from '../api';
 import Home from '../pages/home';
+import Loader from "./loader";
+import styles from '../styles/index.css';
+
+
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
 
   useEffect(() => {
 
     const fetchPosts = async () => {
       const response = await getPosts();
 
+      if( response.success){
+        setPosts(response.data.posts);
+      }
+
+
+      setLoading(false);
       console.log('response', response);
     };
 
@@ -17,10 +32,16 @@ function App() {
 
   }, [])
 
+  if(loading){
+     return <Loader />
+  }
+
   return (
     <div className="App">
       {/* <h1>Karan Kumar!!</h1> */}
 
+      <Home posts={posts} />
+      <Home />
       <Home />
     </div>
   );
